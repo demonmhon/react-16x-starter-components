7995718c88cd14d0d0d59493c1aa4d6232374ea1
Thanawat Petchsri <thanawat.petchsri@gmail.com>
2019-07-22 23:24:35 +0700

    Update packages

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -26,6 +26,16 @@ module.exports = {
             outputPath: 'images'
           }
         }]
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]?[hash]',
+            outputPath: 'fonts'
+          }
+        }]
       }
     ]
   },
