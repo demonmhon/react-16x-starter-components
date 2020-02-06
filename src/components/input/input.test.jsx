b509690822b251c8eefdcb7bdbdf33d5ce9ec063
Thanawat Petchsri <thanawat.petchsri@gmail.com>
2020-02-06 23:21:05 +0700

    Add more test

--- src/components/input/input.test.jsx
+++ src/components/input/input.test.jsx
@@ -4,56 +4,84 @@ import { create } from 'react-test-renderer';
 import Input from './index';
 
 describe('Input component', () => {
-  test('it matches the snapshot', () => {
-    const component = create(<Input />);
-    expect(component.toJSON()).toMatchSnapshot();
-  });
-
-  test('has the provided class name', () => {
-    const className = 'the-custom-css-class';
-    const component = shallow(<Input className={className} />);
-    expect(component.hasClass(className)).toBe(true);
-  });
-
-  describe('attributes', () => {
-    test('value set correctly', () => {
-      const props = {
-        value: 'input value 1234',
-      };
-      const component = mount(<Input {...props} />);
-      expect(component.find('input').props().value).toEqual(props.value);
+  describe('Input', () => {
+    test('it matches the snapshot', () => {
+      const component = create(<Input />);
+      expect(component.toJSON()).toMatchSnapshot();
     });
 
-    test('disabled set correctly', () => {
-      const props = {
-        disabled: true,
-      };
-      const component = mount(<Input {...props} />);
-      expect(component.find('input').props().disabled).toEqual(true);
+    test('has the provided class name', () => {
+      const className = 'the-custom-css-class';
+      const component = shallow(<Input className={className} />);
+      expect(component.hasClass(className)).toBe(true);
+    });
+
+    describe('attributes', () => {
+      test('value set correctly', () => {
+        const props = {
+          value: 'input value 1234',
+        };
+        const component = mount(<Input {...props} />);
+        expect(component.find('input').props().value).toEqual(props.value);
+      });
+
+      test('disabled set correctly', () => {
+        const props = {
+          disabled: true,
+        };
+        const component = mount(<Input {...props} />);
+        expect(component.find('input').props().disabled).toEqual(true);
+      });
+    });
+
+    describe('onChange()', () => {
+      test('called', () => {
+        const props = {
+          onChange: jest.fn(),
+        };
+        const component = mount(<Input {...props} />);
+        component
+          .find('input')
+          .simulate('change', { target: { value: 'new-value' } });
+        expect(props.onChange).toHaveBeenCalledTimes(1);
+      });
+
+      test('not called if disabled', () => {
+        const props = {
+          onChange: jest.fn(),
+        };
+        const component = mount(<Input {...props} />);
+        component
+          .find('input')
+          .simulate('change', { target: { value: 'new-value' } });
+        expect(props.onChange).toHaveBeenCalledTimes(1);
+      });
     });
   });
 
-  describe('onChange()', () => {
-    test('called', () => {
-      const props = {
-        onChange: jest.fn(),
-      };
-      const component = mount(<Input {...props} />);
-      component
-        .find('input')
-        .simulate('change', { target: { value: 'new-value' } });
-      expect(props.onChange).toHaveBeenCalledTimes(1);
-    });
+  describe('Input.Textarea', () => {
+    describe('onChange()', () => {
+      test('called', () => {
+        const props = {
+          onChange: jest.fn(),
+        };
+        const component = mount(<Input.Textarea {...props} />);
+        component
+          .find('textarea')
+          .simulate('change', { target: { value: 'new-value' } });
+        expect(props.onChange).toHaveBeenCalledTimes(1);
+      });
 
-    test('not called if disabled', () => {
-      const props = {
-        onChange: jest.fn(),
-      };
-      const component = mount(<Input {...props} />);
-      component
-        .find('input')
-        .simulate('change', { target: { value: 'new-value' } });
-      expect(props.onChange).toHaveBeenCalledTimes(1);
+      test('not called if disabled', () => {
+        const props = {
+          onChange: jest.fn(),
+        };
+        const component = mount(<Input.Textarea {...props} />);
+        component
+          .find('textarea')
+          .simulate('change', { target: { value: 'new-value' } });
+        expect(props.onChange).toHaveBeenCalledTimes(1);
+      });
     });
-  });
+  })
 });
--- src/components/radio/radio.test.jsx
+++ src/components/radio/radio.test.jsx
@@ -14,4 +14,60 @@ describe('Radio component', () => {
     const component = shallow(<Radio className={className} />);
     expect(component.hasClass(className)).toBe(true);
   });
+
+  test('has the provided class name', () => {
+    const className = 'the-custom-css-class';
+    const component = shallow(<Radio className={className} />);
+    expect(component.hasClass(className)).toBe(true);
+  });
+
+  test('disabled some option', () => {
+    const component = shallow(
+      <Radio.Group>
+        <Radio value="a" label={'Option A'} />
+        <Radio value="b" label={'Option B'} />
+        <Radio value="c" label={'Option C'} disabled />
+        <Radio value="d" label={'Option D'} disabled />
+      </Radio.Group>
+    );
+    expect(component.find('[value="c"]').is('[disabled]')).toBe(true)
+    expect(component.find('[value="d"]').is('[disabled]')).toBe(true)
+  });
+
+  describe('onChange()', () => {
+    test('called', () => {
+      const props = {
+        onChange: jest.fn(),
+      };
+      const component = shallow(
+        <Radio.Group {...props}>
+          <Radio value="a" label={'Option A'} />
+          <Radio value="b" label={'Option B'} />
+          <Radio value="c" label={'Option C'} />
+          <Radio value="d" label={'Option D'} />
+        </Radio.Group>
+      );
+      component.find('[value="c"]').simulate('change', () => {});
+      expect(props.onChange).toHaveBeenCalledTimes(1);
+    });
+
+    test('not called if disabled', () => {
+      const props = {
+        disabled: true,
+        onChange: jest.fn(),
+      };
+      const component = shallow(
+        <Radio.Group {...props}>
+          <Radio value="a" label={'Option A'} />
+          <Radio value="b" label={'Option B'} />
+          <Radio value="c" label={'Option C'} disabled />
+          <Radio value="d" label={'Option D'} disabled />
+        </Radio.Group>
+      );
+      component.find('[value="c"]').simulate('change', () => {});
+      component.find('[value="a"]').simulate('change', () => {});
+      component.find('[value="d"]').simulate('change', () => {});
+      expect(props.onChange).toHaveBeenCalledTimes(0);
+    });
+  });
 });
