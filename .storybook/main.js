819382e6e4149b58371c202cd9d04d5d0974eebd
demonmhon <thanawat.petchsri@gmail.com>
2022-02-18 23:38:22 +0700

    Storybook 5

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,7 @@
 module.exports = {
-  stories: [
-    '../src/**/*.stories.mdx',
-    '../src/**/*.stories.@(js|jsx|ts|tsx)'
-  ],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/preset-scss',
-    '@storybook/addon-postcss',
-    'storybook-dark-mode'
-  ]
-}
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/preset-scss', '@storybook/addon-postcss', 'storybook-dark-mode'],
+  core: {
+    builder: "webpack5"
+  }
+};
